4d00bd2a1f220d5cb86ba267a1ac5ed3d875b139
CGfrac <cgfrac@protonmail.com>
2023-02-14 12:02:32 +0100

    Do part 1d

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,51 +1,60 @@
 import { useState } from "react";
 
-const Display = ({ counter }) => {
+const Display = ({ content }) => {
   return (
-    <div>{counter}</div>
+    <span>{content}</span>
   )
 }
 
-const Button = ({ onClick, text }) => {
+const Button = ({ handleClick, text }) => {
   return (
-    <button onClick={onClick}>
+    <button onClick={handleClick}>
       {text}
     </button>
   )
 }
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>the app is used by pressing the buttons</div>
+    )
+  }
+  return (
+    <div>
+      button press history: {allClicks.join(' ')}
+    </div>
+  )
+}
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+const App = () => {
+  const [ left, setLeft ] = useState(0)
+  const [ right, setRight ] = useState(0)
+  const [ allClicks, setAll ] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
   }
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-  const setToZero = () => {
-    setCounter(0)
-    console.log('resetting to zero, value before', counter)
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
 
   return (
     <div>
-      <Display counter={counter} />
+      <Display content={left} />
       <Button 
-        onClick={increaseByOne} 
-        text="plus" 
+        handleClick={handleLeftClick} 
+        text="left" 
       />
       <Button 
-        onClick={setToZero} 
-        text="zero" 
+        handleClick={handleRightClick} 
+        text="right" 
       />
-      <Button 
-        onClick={decreaseByOne} 
-        text="minus" 
-      />
+      <Display content={right} />
+      <History allClicks={allClicks} />
     </div>
   )
 }
